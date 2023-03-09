28183913861d157f43bb5c909c09a80e0824c819
Eric Echemane <eechemane29@gmail.com>
2023-03-09 22:41:52 +0800

    able to create a file

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,47 @@
 #!/usr/bin/env node
 
 import chalk from 'chalk';
-import boxen from 'boxen';
 import yargs from 'yargs';
+import fs from 'fs';
 import { hideBin } from 'yargs/helpers';
 
 yargs(hideBin(process.argv))
 	.scriptName('cire')
 	.usage('$0 <cmd> [args]')
 	.command(
-		'model [model_name]',
+		'model [model]',
 		'Creates a mongoose model',
 		(yargs) => {
-			yargs.positional('model_name', {
+			yargs.positional('model', {
 				type: 'string',
 				describe: 'the name of the mongoose model',
 			});
 		},
 		function (argv) {
-			if (!argv.model_name) {
-				console.log(chalk.red('You must provide a model name'));
+			const { model } = argv;
+
+			if (!model) {
+				console.log(chalk.redBright('You must provide a model name'));
 				return;
 			}
-			console.log(`You want to create a model called ${argv.model_name}`);
+
+			console.log(chalk.bold(`=> Creating ${model} model...`));
+
+			const currentDir = process.cwd();
+
+			if (!fs.existsSync(currentDir + '/models')) {
+				console.log(chalk.bold(`=> Creating models directory...`));
+				fs.mkdirSync(currentDir + '/models');
+			}
+
+			fs.writeFile(
+				currentDir + `/models/${model.toLowerCase()}.model.ts`,
+				'import mongoose from "mongoose";\n\n',
+				function (err) {
+					if (err) throw err;
+					console.log(chalk.greenBright(`=> ${model} model has been created`));
+				}
+			);
 		}
 	)
 	.help().argv;
