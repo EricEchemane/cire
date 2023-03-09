f3fb3dc39e5ed997613432c9361b1694b0f3fdef
Eric Echemane <eechemane29@gmail.com>
2023-03-09 22:14:20 +0800

    sample command

--- bin/index.js
+++ bin/index.js
@@ -3,15 +3,26 @@
 import chalk from 'chalk';
 import boxen from 'boxen';
 import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
-const welcome = chalk.bold(
-	'WELCOME to CIRE\n\nCIRE is a tool for generating model and CRUD api routes in NextJs with mongoose and react-query'
-);
-const welcomeBox = boxen(welcome, {
-	textAlignment: 'center',
-	padding: 1,
-	margin: 1,
-	float: 'center',
-});
-
-console.log(welcomeBox);
+yargs(hideBin(process.argv))
+	.scriptName('cire')
+	.usage('$0 <cmd> [args]')
+	.command(
+		'model [model_name]',
+		'Creates a mongoose model',
+		(yargs) => {
+			yargs.positional('model_name', {
+				type: 'string',
+				describe: 'the name of the mongoose model',
+			});
+		},
+		function (argv) {
+			if (!argv.model_name) {
+				console.log(chalk.red('You must provide a model name'));
+				return;
+			}
+			console.log(`You want to create a model called ${argv.model_name}`);
+		}
+	)
+	.help().argv;
