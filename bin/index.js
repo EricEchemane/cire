76ece363be9f2651f12ecc81106a0194d100dcd2
Eric Echemane <eechemane29@gmail.com>
2023-03-09 21:36:57 +0800

    add welcome message box

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log('Hello World from Cire');
+import chalk from 'chalk';
+import boxen from 'boxen';
+import yargs from 'yargs';
+
+const welcome = chalk.bold(
+	'WELCOME to CIRE\n\nCIRE is a tool for generating model and CRUD api routes in NextJs with mongoose and react-query'
+);
+const welcomeBox = boxen(welcome, {
+	textAlignment: 'center',
+	padding: 1,
+	margin: 1,
+	float: 'center',
+});
+
+console.log(welcomeBox);
